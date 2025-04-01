2d7301ba3d0687df72af6a13ab74cbb8c3f7d8f4
varsha <varsha@karmadhi.com>
2025-04-01 10:03:27 +0530

    our people

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -20465,7 +20465,73 @@ var Parts = /*#__PURE__*/function () {
   }
   parts_createClass(Parts, [{
     key: "init",
-    value: function init() {}
+    value: function init() {
+      this.vacancies();
+    }
+  }, {
+    key: "vacancies",
+    value: function vacancies() {
+      $(document).ready(function () {
+        $('.vacancies-content').each(function () {
+          var $content = $(this);
+          var defaultHeight = $(window).width() <= 393 ? 180 : $(window).width() <= 426 ? 150 : $(window).width() <= 768 ? 80 : 120;
+          var fullHeight = $content[0].scrollHeight;
+
+          // Set initial max-height and overflow
+          $content.css({
+            'max-height': defaultHeight + 'px',
+            'overflow': 'hidden'
+          });
+
+          // Check if content is larger than default height
+          if (fullHeight > defaultHeight) {
+            $content.after('<div class="vacancies-btn rale-medium font16 leading26 text-decoration-underline text-0D2033 cursor-pointer">Read more</div>');
+          }
+        });
+
+        // Read more/less button click event
+        $(document).on('click', '.vacancies-btn', function () {
+          var $button = $(this);
+          var $content = $button.prev('.vacancies-content');
+          var fullHeight = $content[0].scrollHeight;
+          var defaultHeight = $(window).width() <= 393 ? 180 : $(window).width() <= 426 ? 150 : $(window).width() <= 768 ? 80 : 120;
+
+          // Close all other content before opening the clicked one
+          $('.vacancies-content').not($content).each(function () {
+            $(this).removeClass('active').animate({
+              'max-height': defaultHeight + 'px'
+            }, 500);
+            $(this).next('.vacancies-btn').text('Read More');
+          });
+
+          // Toggle the clicked content
+          if ($content.hasClass('active')) {
+            $content.removeClass('active').animate({
+              'max-height': defaultHeight + 'px'
+            }, 500);
+            $button.text('Read More');
+          } else {
+            $content.addClass('active').animate({
+              'max-height': fullHeight + 'px'
+            }, 500);
+            $button.text('Read Less');
+          }
+        });
+
+        // Window resize event
+        $(window).resize(function () {
+          $('.vacancies-content').each(function () {
+            var $content = $(this);
+            var defaultHeight = $(window).width() <= 393 ? 180 : $(window).width() <= 426 ? 150 : $(window).width() <= 768 ? 80 : 120;
+            if (!$content.hasClass('active')) {
+              $content.css({
+                'max-height': defaultHeight + 'px'
+              });
+            }
+          });
+        });
+      });
+    }
   }]);
   return Parts;
 }();
